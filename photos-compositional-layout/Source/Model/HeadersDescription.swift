7de30d1541df73c31d678fd84f442af4d1d12f0a
ti1ek <tilek.koszhanov@mail.ru>
2023-03-16 16:50:29 +0500

    MyAlbums Cell is added

---
+++ photos-compositional-layout/Source/Model/HeadersDescription.swift
@@ -0,0 +1,13 @@
+//
+//  HeadersDescription.swift
+//  photos-compositional-layout
+//
+//  Created by Zhuldyz Bukeshova on 16.03.2023.
+//
+
+import Foundation
+
+struct Header {
+    var title: String
+    var subtitle: String?
+}
---
+++ photos-compositional-layout/Source/ViewControllers/SearchController.swift
@@ -0,0 +1,16 @@
+//
+//  SearchController.swift
+//  photos-compositional-layout
+//
+//  Created by Zhuldyz Bukeshova on 16.03.2023.
+//
+
+import UIKit
+
+class SearchController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
