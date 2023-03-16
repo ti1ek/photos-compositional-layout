5a3837218544c0d30fa441c3a975aa64f11b3db1
ti1ek <tilek.koszhanov@mail.ru>
2023-03-16 16:04:54 +0500

    Initial Commit

---
+++ photos-compositional-layout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  photos-compositional-layout
+//
+//  Created by Zhuldyz Bukeshova on 16.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
